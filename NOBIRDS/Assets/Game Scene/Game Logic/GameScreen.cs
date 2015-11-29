ce1dbeedc7477f6336a9bdb5fa8ff01ed5b10361
btosun <berktosn@gmail.com>
2015-11-29 18:41:25 +0200

    GameScreen.cs updated

--- NOBIRDS/Assets/Game Scene/Game Logic/GameScreen.cs
+++ NOBIRDS/Assets/Game Scene/Game Logic/GameScreen.cs
@@ -1,15 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameScree : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class GameScreen : MonoBehaviour {
+	public Camera mainCamera;
+	public static float minimumXOfScreen = 0f; 
+	public static float maximumXOfScreen = 0f; 
+	public static float minimumYOfScreen = 0f; 
+	public static float maximumYOfScreen = 0f; 
+	public static float groundZ = 0f; 
 	
+	private static float creationSpace = 1f; 
+	
+	public static float minX = minimumXOfScreen - creationSpace; 
+	public static float maxX = maximumXOfScreen + creationSpace; 
+	public static float minY = minimumYOfScreen - creationSpace; 
+	public static float maxY = maximumYOfScreen + creationSpace; 
+	
+	public static float centralX = minimumXOfScreen + (maximumXOfScreen - minimumXOfScreen) / 2f; 
+	public static float centralY = minimumYOfScreen + (maximumYOfScreen - minimumYOfScreen) / 2f;
+	public float mouseX;
+	public float mouseY;
+	
+	void FixedUpdate()
+	{
+		mouseX = mainCamera.ScreenToWorldPoint(Input.mousePosition).x - centralX;
+		mouseY = mainCamera.ScreenToWorldPoint(Input.mousePosition).z - c-AentralY;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public static float getSolutionWidth()
+	{
+		return 0;
 	}
+	
+	public static float getSolutionHeight()
+	{
+		return 0;
+	}
+	
+	public static float getWidth () { 
+		return maxX - minX;
+	}
+	
+	public static float getHeight () {
+		return maxY - minY;
+	}
+	
+	public static Vector3 getRandomVec3FromLeft () {
+		return new Vector3 (minX, getRandomY (), groundZ); 
+	}
+	
+	public static Vector3 getRandomVec3FromRight () {
+		return new Vector3 (maxX, getRandomY (), groundZ); 
+	}
+	
+	public static float getRandomX () {
+		return Random.Range (minX, maxX);
+	}
+	
+	public static float getRandomY () {
+		return Random.Range (minY, maxY);
+	} 
 }
