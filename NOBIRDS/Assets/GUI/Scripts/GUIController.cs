02755d71370eed3aaa7ea77b36b0a5de9e366495
btosun <berktosn@gmail.com>
2015-12-04 14:57:00 +0200

    GUIController added

--- NOBIRDS/Assets/GUI/Scripts/GUIController.cs
+++ NOBIRDS/Assets/GUI/Scripts/GUIController.cs
@@ -2,14 +2,57 @@
 using System.Collections;
 
 public class GUIController : MonoBehaviour {
-
+    private GameGUI view; 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        view = gameObject.GetComponent<GameGUI>();  
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+      
+    }
+
+    public void playButtonClicked()
+    {
+		Application.LoadLevel("Game Scene");
+    }
+
+    public void hotToPlayButtonClicked()
+    {
+        view.showHowToPlay();
+    }
+
+    public void highScoresButtonClicked()
+    {
+        view.showHighScores();
+    }
+
+    public void aboutUsButtonClicked()
+    {
+        view.showAboutUs();
+    }
+
+    public void okButtonClicked()
+    {
+        view.showMainMenu();
+    }
+
+    public void pauseButtonClicked()
+    {
+        GameLogic.setPaused(!GameLogic.pause);
+    }
+
+    public void muteButtonClicked()
+    {
+        GameLogic.setMuted(!GameLogic.mute);
+    }
+     
+    public void highScoreNameEntered() 
+    {
+        GameLogic.recordHighScore(view.highScoreNameText.text);
+        view.showMainMenu();
+    }
+
 }
